4d0993c1d6510a97a6b791aa0585414e9d19dfec
Sidigi <sidigicoder@gmail.com>
2020-08-24 14:28:51 +0500

    wip

--- config/laravel-remote-models.php
+++ config/laravel-remote-models.php
@@ -1,11 +1,47 @@
 <?php
 
 return [
+    /*
+    |--------------------------------------------------------------------------
+    | Default Options
+    |--------------------------------------------------------------------------
+    |
+    | This option controls the default response key
+    |
+    */
     'options' => [
         'response_key' => 'data',
     ],
 
+    /*
+    |--------------------------------------------------------------------------
+    | Remote Clients
+    |--------------------------------------------------------------------------
+    |
+    |    'comment-client' => [
+    |        'client' =>  App\Clients\CommentClient::class,
+    |        'base_uri' => 'base uri',
+    |        'paths' => [
+    |        ],
+    |    ],
+    |
+    |
+    */
     'clients' => [
+
+    ],
+
+    /*
+    |--------------------------------------------------------------------------
+    | Remote Models
+    |--------------------------------------------------------------------------
+    |
+    |   App\RemoteModels\Comment::class => 'comment-client',
+    |   //or
+    |   App\RemoteModels\Comment::class => App\Clients\CommentClient::class,
+    |
+    */
+    'models' => [
 
     ],
 ];
