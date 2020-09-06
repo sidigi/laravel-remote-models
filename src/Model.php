5461a39f019819a37a6246f37b05287837a73ca1
Sidigi <sidigicoder@gmail.com>
2020-09-06 12:17:55 +0500

    Add extending from base eloquent model

--- src/Model.php
+++ src/Model.php
@@ -2,26 +2,12 @@
 
 namespace Sidigi\LaravelRemoteModels;
 
-use ArrayAccess;
-use Illuminate\Contracts\Support\Arrayable;
-use Illuminate\Contracts\Support\Jsonable;
-use Illuminate\Database\Eloquent\Concerns\GuardsAttributes;
-use Illuminate\Database\Eloquent\Concerns\HasAttributes;
-use Illuminate\Database\Eloquent\Concerns\HidesAttributes;
-use Illuminate\Database\Eloquent\JsonEncodingException;
-use Illuminate\Database\Eloquent\MassAssignmentException;
+use Illuminate\Database\Eloquent\Model as EloquentModel;
 use Illuminate\Support\Collection;
-use Illuminate\Support\Traits\ForwardsCalls;
-use JsonSerializable;
-use Sidigi\LaravelRemoteModels\Concerns\HasRelationships;
 use Sidigi\LaravelRemoteModels\Exceptions\ClientNotFoundException;
 
-abstract class Model implements Arrayable, ArrayAccess, Jsonable, JsonSerializable
+abstract class Model extends EloquentModel
 {
-    use HasAttributes, ForwardsCalls, GuardsAttributes, HidesAttributes, HasRelationships;
-
-    public $timestamps = false;
-
     protected function getClientClass() : string
     {
         if (! $client = config('laravel-remote-models.models.'.static::class)) {
@@ -40,13 +26,6 @@ abstract class Model implements Arrayable, ArrayAccess, Jsonable, JsonSerializab
         return resolve($this->getClientClass());
     }
 
-    public function __construct(array $attributes = [])
-    {
-        $this->syncOriginal();
-
-        $this->fill($attributes);
-    }
-
     public static function query()
     {
         return (new static)->newQuery();
@@ -60,145 +39,5 @@ abstract class Model implements Arrayable, ArrayAccess, Jsonable, JsonSerializab
     public function newCollection(array $models = [])
     {
         return new Collection($models);
-    }
-
-    public function newInstance($attributes = [])
-    {
-        $model = new static((array) $attributes);
-
-        $model->mergeCasts($this->casts);
-
-        return $model;
-    }
-
-    public function newFromBuilder($attributes = [])
-    {
-        $model = $this->newInstance([], true);
-
-        $model->setRawAttributes((array) $attributes, true);
-
-        return $model;
-    }
-
-    public function fill(array $attributes)
-    {
-        $totallyGuarded = $this->totallyGuarded();
-
-        foreach ($this->fillableFromArray($attributes) as $key => $value) {
-            if ($this->isFillable($key)) {
-                $this->setAttribute($key, $value);
-            } elseif ($totallyGuarded) {
-                throw new MassAssignmentException(sprintf(
-                    'Add [%s] to fillable property to allow mass assignment on [%s].',
-                    $key,
-                    get_class($this)
-                ));
-            }
-        }
-
-        return $this;
-    }
-
-    public function syncOriginal()
-    {
-        $this->original = $this->getAttributes();
-
-        return $this;
-    }
-
-    public function getCasts()
-    {
-        return $this->casts;
-    }
-
-    public function usesTimestamps()
-    {
-        return $this->timestamps;
-    }
-
-    //implementation interfaces
-    public function toArray()
-    {
-        return $this->attributesToArray();
-    }
-
-    public function jsonSerialize()
-    {
-        return $this->toArray();
-    }
-
-    public function offsetExists($offset)
-    {
-        return ! is_null($this->getAttribute($offset));
-    }
-
-    public function offsetGet($offset)
-    {
-        return $this->getAttribute($offset);
-    }
-
-    public function offsetSet($offset, $value)
-    {
-        $this->setAttribute($offset, $value);
-    }
-
-    public function offsetUnset($offset)
-    {
-        unset($this->attributes[$offset]);
-    }
-
-    public function toJson($options = 0)
-    {
-        $json = json_encode($this->jsonSerialize(), $options);
-
-        if (JSON_ERROR_NONE !== json_last_error()) {
-            throw JsonEncodingException::forModel($this, json_last_error_msg());
-        }
-
-        return $json;
-    }
-
-    public function __sleep()
-    {
-        $this->mergeAttributesFromClassCasts();
-
-        $this->classCastCache = [];
-
-        return array_keys(get_object_vars($this));
-    }
-
-    public function __toString()
-    {
-        return $this->toJson();
-    }
-
-    public function __isset($key)
-    {
-        return $this->offsetExists($key);
-    }
-
-    public function __unset($key)
-    {
-        $this->offsetUnset($key);
-    }
-
-    public function __get($key)
-    {
-        return $this->getAttribute($key);
-    }
-
-    public function __set($key, $value)
-    {
-        $this->setAttribute($key, $value);
-    }
-
-    public function __call($method, $parameters)
-    {
-        return $this->forwardCallTo($this->newQuery(), $method, $parameters);
-    }
-
-    public static function __callStatic($method, $parameters)
-    {
-        return (new static)->$method(...$parameters);
     }
 }
